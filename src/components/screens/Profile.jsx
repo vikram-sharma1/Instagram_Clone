67a08e951195560c7926518955c4361dc51b9df4
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-03-29 22:53:02 +0530

    dummy profile page

--- src/components/screens/Profile.jsx
+++ src/components/screens/Profile.jsx
@@ -1,7 +1,33 @@
 
 const Profile = () => {
     return (
-        <h1>Profile</h1>
+        <div>
+            {/* Profile Image & Profile Image / */}
+            <div className="imgNamediv">
+                <div> 
+                    <img src="https://images.unsplash.com/photo-1593085512500-5d55148d6f0d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" alt="Profile Images" className="profileImgV" />
+                </div>
+                <div>
+                    <h1>Demo Name</h1>
+                    <p><b>Dummy text for profile bio</b> <br />  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium nesciunt qui doloribus? Ab libero animi perferendis obcaecati corrupti officiis, neque vel tempora error, nulla, excepturi qui beatae minima possimus dolor et esse. Accusamus, quia quasi a culpa enim rerum fugit.</p>
+                </div>
+            </div>
+
+            {/* Boxes for Gallery images  */}
+
+           <div className="GalleryBoxV">
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+               <img src="https://images.unsplash.com/photo-1620428268482-cf1851a36764?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8Y2FydG9vbnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="Gallery Image" className="PicItemV" />
+           </div>
+
+        </div>
     )
 }
 
