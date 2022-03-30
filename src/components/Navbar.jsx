188d09a1e3869d423c3db1d25ad196431ad1b48f
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-03-30 11:34:52 +0530

    postion change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,10 +7,10 @@ const Navbar = () => {
         <div className="nav-wrapper white navV" >
             <Link to="/" className="brand-logo left">Instagram</Link>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
+            <li><Link to="/createPost">Create Post</Link></li>
             <li><Link to="/profile">Profile</Link></li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/signup">Sign up</Link></li>
-            <li><Link to="/createPost">Create Post</Link></li>
             </ul>
         </div>
     </nav>
