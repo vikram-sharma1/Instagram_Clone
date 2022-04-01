16b5c4cece8aaadfcb289e459648b7e60a06f449
Vikram Sharma <thevikramdeveloper@gmail.com>
2022-04-01 12:24:36 +0530

    new signup page

--- src/components/screens/Signup.jsx
+++ src/components/screens/Signup.jsx
@@ -1,53 +1,55 @@
 
 const Signup = () => {
     return (
-        <div className="mycard">
-        <div className="card auth-card input-field">
-          <h2>Instagram</h2>
-          <input
-          type="text"
-          placeholder="name"
-          //value={name}
-          //onChange={(e)=>setName(e.target.value)}
-          />
-          <input
-          type="text"
-          placeholder="email"
-          //value={email}
-          //onChange={(e)=>setEmail(e.target.value)}
-          />
-          <input
-          type="password"
-          placeholder="password"
-          //value={password}
-          //onChange={(e)=>setPasword(e.target.value)}
-          />
-          <div className="file-field input-field">
-          {/* <div className="btn #64b5f6 blue darken-1">
-              <span>Upload pic</span>
-              <input type="file" 
-              onChange={(e)=>setImage(e.target.files[0])} 
-              />
-          </div> */}
-          {/* <div className="file-path-wrapper">
-              <input className="file-path validate" type="text" />
-          </div> */}
-          </div>
-          <button className="btn waves-effect waves-light #64b5f6 blue darken-1"
-          //onClick={()=>PostData()}
-          >
-              SignUP
-          </button>
-          {/* <h5>
-              <Link to="/signin">Already have an account ?</Link>
-          </h5> */}
-           
-             
-       
-          
-  
-      </div>
-    </div>
+        <div className="signUpBoxV">
+             <div className="rightboxVdetails">
+				 <div className="instaV">
+					 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/800px-Instagram_logo.svg.png?20160616034027" alt=""  />
+				 </div>
+                 <div>
+                     <p className="signdetV">Sign up to see photos and videos from your friends.</p>
+                 </div>
+                 <div className="loginfbVV">
+						<img src="https://i.pinimg.com/564x/b3/26/b5/b326b5f8d23cd1e0f18df4c9265416f7--facebook-icon-negative-feedback.jpg" className="img-fluid rounded" alt="[+]" />
+							<div>Login with Facebook</div>
+				</div>
+               {/* ` <div className="or">
+					<div></div>
+					<div>OR</div>
+					<div></div>
+				</div>` */}
+
+					<form>
+                    <div className="inputoneV" >
+						<input type="text" placeholder="Mobile Number or Email" />
+						</div>
+                        <div className="inputoneV" >
+						<input type="text" placeholder="Full Name" />
+						</div>
+                        <div className="inputoneV" >
+						<input type="text" placeholder="Username" />
+						</div>
+						<div className="inputoneV" >
+						<input type="password" placeholder="Password"  />
+						</div>
+						<div className="inputoneV" >
+						<input type="submit" value="Log In" className="InpButtV"  />
+						</div>
+					</form>
+                <div className="termscondV">
+                    <p>By signing up, you agree to our <b>Terms , Data Policy and Cookies Policy .</b></p>
+                </div>
+				
+				<div className="otherMethods">
+					
+				</div>
+				{/* <div className="forget">Forgot Password ?</div> */}
+				<div className="forget top">
+					<p>Have an account? <b style={{color:"blue"}}>Log In</b></p>
+				</div>
+
+			</div>
+        </div>
     )
 }
 
